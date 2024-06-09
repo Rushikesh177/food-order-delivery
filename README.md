408c59c090bb68936294f69ee0f14abfee41ea86
Rushikesh177 <rushiwaghule17@gmail.com>
2024-06-09 05:04:08 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# 🥘 Food Order Website
+
+# ⚙️ Technology Used
+1. HTML5
+2. CSS3
+3. PHP 
+4. MySQL 
+
+
+
+
+
+
+
+
+
+
+
---
+++ partials-front/footer.php
@@ -0,0 +1,26 @@
+
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.github.com/Rushikesh177/" target="_blank"><img src="https://img.icons8.com/fluent/50/000000/github.png"/></a>
+                </li>
+                <li>
+                    <a href="#" target="_blank"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/Rushikesh177" target="_blank"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+  
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Rushikesh Waghule</a></p>
+        </div>
+    </section>
+    
+
+</body>
+</html>
